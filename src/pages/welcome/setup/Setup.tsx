c3b1279921d9c2e51f98f239066f0aa60685e083
Kevin Wong <kev.jen.wong@googlemail.com>
2022-07-25 22:20:22 -0700

    added nullchecks and references

--- src/pages/welcome/setup/Setup.tsx
+++ src/pages/welcome/setup/Setup.tsx
@@ -1,7 +1,63 @@
 import React from 'react';
-import { Button, Center, List, Title, Space } from '@mantine/core';
+import { useState } from 'react';
+import { useRef } from 'react';
+import { NumberInput, Button, Center, List, Title, Space, TextInput } from '@mantine/core';
 
 function Setup() {
+
+  const [step, setStep] = useState(1);
+  let title;
+  let description;
+  let entry;
+
+  const nameRef = useRef<HTMLInputElement>(null);
+
+  switch(step) {
+    case 1: {
+      title = "Server Name";
+      description = "Choose a name for your server.";
+      entry = (    
+        <TextInput
+          placeholder="my-first-server"
+          label="Name"
+          ref={nameRef}
+          required
+        />
+      );
+      break;
+    }
+    case 2: {
+      title = "Specifications";
+      description = "Specify the amount of RAM to allocate.";
+      entry = (
+        <NumberInput
+          defaultValue={4}
+          placeholder="4"
+          label="RAM"
+          description="Value in Gigabytes. Recommend min 4gb."
+          required
+          max={32}
+          min={2}
+        />
+      );
+      break;
+    }
+  }
+
+  function clickEvent() {
+    if(step >= 4) {
+      return;
+    }
+    if(step === 1) {
+      if (nameRef.current != null) {
+        if (nameRef.current.value === "") {
+          return;
+        }
+      }
+    }
+    setStep(step+1);
+  }
+
   return (
     <div className="setup">
       <Center style={{ width: '100%', height: '75vh' }}>
@@ -9,14 +65,14 @@ function Setup() {
           <List.Item><Title order={1}>Getting Started</Title></List.Item>
           <List.Item>Let's set up your first server!</List.Item>
           <Space h="md" />
-          <List.Item><Title order={3}>Step [Step]: [Title]</Title></List.Item>
-          <List.Item>[Description]</List.Item>
+          <List.Item><Title order={3}>Step {step}: {title}</Title></List.Item>
+          <List.Item>{description}</List.Item>
           <Space h="md" />
-          <List.Item>
-            [Entry]
+          <List.Item>    
+            {entry}
           </List.Item>
           <Space h="md" />
-          <Button variant="default" compact>
+          <Button variant="default" compact onClick={clickEvent}>
             Next 
           </Button>
         </List>
